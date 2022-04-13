ef4e4767dd2b92f6ca7ec213cefe8304b1e60ac3
Milkey Tan <819774989@qq.com>
2022-04-13 11:10:25 +0800

    DnsSpoofCheck.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify
 import MPing.ICMPing
 import MPing.TCPing
 import MDns.DnsQuery
+import MDns.DnsSpoofCheck
 
 app = Flask(__name__)
 wsgi_app = app.wsgi_app
@@ -15,9 +16,13 @@ def hello_world():
     return "<p>Welcome to Eris!</p>"
 
 
-@app.route('/dns/<name>')
+@app.route('/dns/query/<name>')
 def DNSQuery(name):
     return jsonify(MDns.DnsQuery.Query(name))
+
+@app.route('/dns/check/<name>')
+def DNSCheck(name):
+    return jsonify(MDns.DnsSpoofCheck.Check(name))
 
 @app.route('/ping/icmp/<ip>')
 def ICMPing(ip):
