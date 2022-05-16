27a546e03509ff38e6edf7200c2d73f3729804b7
Milkey Tan <819774989@qq.com>
2022-05-16 13:59:19 +0800

    DNSUI

--- DnsUI.py
+++ DnsUI.py
@@ -1,14 +1,9 @@
-import datetime
-import time
-
 import flag
 import geoip2
-from pyecharts import options as opts
-from pyecharts.charts import Line
 from pywebio.input import *
 from pywebio.output import *
 
-from MNetwork import MPing, MDns
+from MNetwork import MDns, MHost
 
 
 def mdns():
@@ -31,11 +26,20 @@ def mdns():
                 put_html(open("spin.html", "r").read())
                 toast("正在进行查询……")
 
-            table = [["状态", "响应代码", "返回数据", "返回类型"]]
+            table = [["状态", "响应代码", "返回数据", "返回类型", "地理位置", "ISP"]]
 
             res = MDns.Dns(target["host"]).Query()
 
-            table.append([res["state"], res["rcode"], res["rdata"], res["type"]])
+            if MHost.Host(res["rdata"]).isIp():
+                r = cityReader.city(res["rdata"])
+                n = asnReader.asn(res["rdata"])
+                loc = flag.flag(str.upper(r.country.iso_code)) + " " + r.country.iso_code + \
+                      r.subdivisions.most_specific.name if r.subdivisions.most_specific.name is not None else "" + \
+                      r.city.name if r.city.name is not None else ""
+                isp = "AS" + str(n.autonomous_system_number) + " " + n.autonomous_system_organization
+                table.append([res["state"], res["rcode"], res["rdata"], res["type"], loc, isp])
+            else:
+                table.append([res["state"], res["rcode"], res["rdata"], res["type"], "", ""])
 
             toast("查询完成！", color="success")
             clear("spin")
